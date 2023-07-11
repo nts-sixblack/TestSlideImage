1f46bd023944155fd3bf28bb8f457d77423511f8
SixBlack <nts.sixblack@gmail.com>
2023-07-12 03:27:34 +0700

    Initial Commit

---
+++ TestSlideImage/TestSlideImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSlideImageApp.swift
+//  TestSlideImage
+//
+//  Created by Thanh Sau on 12/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestSlideImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
